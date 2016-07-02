efa32c2821cce025bd48113c6e09f70336ddaa73
Shahjalal Hossain <shossainfolk@gmail.com>
2016-07-02 11:00:48 +0600

    update test case method name

--- crud_app/tests.py
+++ crud_app/tests.py
@@ -9,7 +9,7 @@ class UserTest(TestCase):
     def create_user(self, email="shahjalal.tipu@gmail.com", first_name="Shahjalal", last_name="Hossain", street="Kamal Road", house_no="112", postal_code="1215", town="Dhaka", country="Bangladesh", newsletter="yes"):
         return User.objects.create(email=email, first_name=first_name, last_name=last_name, street=street, house_no=house_no, postal_code=postal_code, town=town, country=country, newsletter=newsletter, created_date=timezone.now(), modified_date=timezone.now())
 
-    def test_whatever_creation(self):
+    def test_user_creation(self):
         u = self.create_user()
         self.assertTrue(isinstance(u, User))
         self.assertEqual(u.email, "shahjalal.tipu@gmail.com")
