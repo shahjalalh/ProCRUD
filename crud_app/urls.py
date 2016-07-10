1bb0061cc64a1eff2509523a99cfd55bf19d90ed
Shahjalal Hossain <shossainfolk@gmail.com>
2016-07-10 13:32:31 +0600

    updated crud app

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.user_info, name='user_info'),
+    url(r'^$', views.user_list, name='user_list'),
+    url(r'^user/user_info/$', views.user_info, name='user_info'),
+    url(r'^user/(?P<pk>\d+)/$', views.user_detail, name='user_detail'),
+    url(r'^user/(?P<pk>\d+)/update/$', views.user_update, name='user_update'),
 ]
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .forms import UserForm
+from .models import User
 
 # Create your views here.
+def user_list(request):
+    users = User.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'user_info/user_list.html', {'users': users})
+
+def user_detail(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    return render(request, 'user_info/user_detail.html', {'user': user})
+
+def user_update(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.author = request.user
+            user.modified_date = timezone.now()
+            user.save()
+            return redirect('user_detail', pk=user.pk)
+    else:
+        form = UserForm(instance=user)
+    return render(request, 'user_info/user_info.html', {'form': form})
+
 def user_info(request):
 
     if request.method == "POST":
@@ -11,7 +34,7 @@ def user_info(request):
             user = form.save(commit=False)
             user.modified_date = timezone.now()
             user.save()
-            return redirect('user_info')
+            return redirect('user_list')
     else:
         form = UserForm()
     return render(request, 'user_info/user_info.html', {'form': form})
